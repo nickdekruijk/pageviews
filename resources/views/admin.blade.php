2fb18cd3c6efbab1e543e33d2707004524479500
Nick de Kruijk <git@nickdekruijk.nl>
2019-02-11 14:45:50 +0100

    Better CSS

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -9,12 +9,12 @@
             <h2>Top urls</h2>
             @include('pageviews::urls')
             <style>
-                A {color:inherit}
-                A:hover {text-decoration:underline}
-                TABLE.ellipsis {width:100%;max-width:700px}
-                TABLE.ellipsis TD:first-child {text-overflow:ellipsis;white-space:nowrap;overflow:hidden;max-width:0;font-size:0.8em}
-                TABLE.ellipsis TD:last-child {width:1%;text-align:right}
-                }
+                .fullpage SELECT {display:inline-block;margin:0;padding:4px 6px;background-color:rgba(255,255,255,0.5);box-sizing:border-box;font:inherit;border-radius:3px;border:1px solid rgba(0,0,0,0.2)}
+                .fullpage A {color:inherit}
+                .fullpage A:hover {text-decoration:underline}
+                .fullpage TABLE.ellipsis {width:100%;max-width:700px}
+                .fullpage TABLE.ellipsis TD:first-child {text-overflow:ellipsis;white-space:nowrap;overflow:hidden;max-width:0;font-size:0.8em}
+                .fullpage TABLE.ellipsis TD:last-child {width:1%;text-align:right}
             </style>
         </section>
 @endsection
